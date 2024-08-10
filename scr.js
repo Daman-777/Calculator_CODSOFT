bac90d66c43078ecf86f17d381b7f81b273fa759
Daman-777 <damanossie68@gmail.com>
2024-08-10 11:25:59 +0530

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,30 @@
+const display = document.querySelector("#disp");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Empty!";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
+    }
+  };
+});
+
+const themeToggleBtn = document.querySelector(".th-togg");
+const calculator = document.querySelector(".cal");
+const toggleIcon = document.querySelector(".tog-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
